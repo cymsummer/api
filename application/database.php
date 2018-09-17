6b355b6d845574041a1d290ba445f4cef678e4e5
summer <626379451@qq.com>
2018-09-17 18:17:24 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.106.20.103',
     // 数据库名
     'database'        => 'luobo',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => 'cym@0908',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
