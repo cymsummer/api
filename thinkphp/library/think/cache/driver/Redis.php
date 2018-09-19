676fd3a8ba51fd7cc4290232072e2baf7a9ac33d
root <13041203572@163.com>
2018-09-19 10:56:41 +0800

    modified:   thinkphp/library/think/cache/driver/Redis.php

--- thinkphp/library/think/cache/driver/Redis.php
+++ thinkphp/library/think/cache/driver/Redis.php
@@ -23,7 +23,7 @@ use think\cache\Driver;
 class Redis extends Driver
 {
     protected $options = [
-        'host' => '127.0.0.1',
+        'host' => '39.106.20.103',
         'port' => 6379,
         'password' => '',
         'select' => 0,
